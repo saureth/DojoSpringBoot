5f80d6637454680d67bbd20a8337c4f64c3b379a
saureth <santiago.sanmartin@udea.edu.co>
2018-03-10 09:54:07 -0500

    Final

---
+++ dojo-spring/src/main/java/co/edu/udea/dojospring/controller/PostController.java
@@ -0,0 +1,56 @@
+package co.edu.udea.dojospring.controller;
+
+
+import co.edu.udea.dojospring.exception.ResourceNotFoundException;
+import co.edu.udea.dojospring.model.Post;
+import co.edu.udea.dojospring.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+@PostController
+@RequestMapping("/api")
+public class PostController{
+
+	@Autowired
+	PostRepository post;
+
+	@GetMapping("/posts")
+	public List<Post> getAllPosts(){
+		return post.findAll();
+	}
+
+	@PostMapping("/post")
+	public Post createPost(@Valid @RequestBody Post post){
+		return this.post.Save(post);
+	}
+
+	@GetMapping("/post/{id}")
+	public Post getPostById(@PathVariable(value = "id") Long postId){
+		return post.findById(postId).orElseThrow(()=> new ResourceNotFoundException("Post", "id", postId));
+	}
+
+	@PutMapping("/post/{id}")
+	public Post updatePost(@PathVariable(value = "id") Long PostId, @Valid @RequestBody post postDetails){
+		Post postNote = post.findById(postId);
+		postNote.setTitle(postDetails.getTitle());
+		postNote.setContent(postDetails.getContent());
+		return post.save(postNote);
+	}
+	
+	@DeleteMapping("/post/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable(value = "id") Long postId) {
+        Post post = this.post.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        this.post.delete(post);
+
+        return ResponseEntity.ok().build();
+    }
+}
